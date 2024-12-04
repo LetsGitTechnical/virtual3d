7395c20fa4b8e3c16c1dad13b72d1d619a55eddd
LetsGitTechnical <98045024+LetsGitTechnical@users.noreply.github.com>
2024-12-04 15:20:42 -0500

    initialized the game, game window, and initialized web cam

--- objectoriented.py
+++ objectoriented.py
@@ -164,11 +164,11 @@ class Stage: #often called in video game programming
    self.cam_h = 720
    self.cam_w = 1280
    self.save_x = 960 #x position being saved between frame
+
  def draw_target_xy(self, img, pos, size): #draws target in appropiate position given users position
-   tx, ty, tz = pos
+
    cv2.circle(img, pos, size, (0, 0, 255), -1) #color is white and -1 is a full circle
-           
-   
+            
    cv2.circle(img, pos, int(size*.8), (255, 255, 255), -1)
 
    cv2.circle(img, pos, int(size *.6), (0, 0, 255), -1)
@@ -190,65 +190,109 @@ class Stage: #often called in video game programming
     x,y= facexy
     e = .9 # smoothing constant
     x = e * x + (1-e)*self.save_x
-    img = np.zros([1080,1920,3])
-    decay = 3
+    img = np.zeros([1080,1920,3])
+    decay = .3
     sx = sy = 0
     dx = int((x - self.cam_w/2)*2)
     for i in range(1,7):
-     sx = sx + int((960-sx)*decay)
-     sy = sy + int((540-sy)*decay)
-     dx = int(dx * decay)
-     #print(sx, sy)
-     cv2.rectangle(img, (sx+dx,sy),(1920-sx+dx, 1080-sy), (255,255,255), 1)
+      sx = sx + int((960-sx)*decay)
+      sy = sy + int((540-sy)*decay)
+      dx = int(dx * decay)
+      #print(sx, sy)
+      cv2.rectangle(img, (sx+dx,sy),(1920-sx+dx, 1080-sy), (255,255,255), 1)
 
-     ball0x = 600+ nt((x - self.cam_w/2)*2*.6)
-     ball0y = 540
+      ball0x = 600+ int((x - self.cam_w/2)*2*.6)
+      ball0y = 540
 
-     cv2.line(img,(960+ int((600-960)*.3**2), 540),(ball0x, ball0y), (255,0,0),3)
-     self.draw_target_xy(img, (ball0x, ball0y), 35)
+      cv2.line(img,(960+ int((600-960)*.3**2), 540),(ball0x, ball0y), (255,0,0),3)
+      self.draw_target_xy(img, (ball0x, ball0y), 35)
 
-     ball1x = 1000+ int((x - self.cam_w/2)*2*.2)
-     ball1y = 440
+      ball1x = 1000+ int((x - self.cam_w/2)*2*.2)
+      ball1y = 440
 
-     cv2.line(img, (960+ int((1200-960)*.3**2), 540 - int((540-650)*.3**2))(ball2x,ball2y),(255,0,0),3)
+      cv2.line(img,  
+               (960+ int((1200-960)*.3**2), 540 - int((540-340)*.3**2)),
+               (ball1x,ball1y),
+               (255,0,0),3)
 
-     self.draw_target_xy(img, (ball2x, ball2y), 50)
+      self.draw_target_xy(img, (ball1x, ball1y), 25)
 
-    cv2.imshow('Ronnals Game', img)
+      ball2x = 1100+ int((x- self.cam_w/2)*2*.9)
+      ball2y = 650
+      
+      cv2.line(img, 
+               (960+ int((1100-960)*.3**2), 540 - int((540-650)*.3**2)),
+               (ball2x, ball2y), 
+               (255,0,0),3)
+
+
+      self.draw_target_xy(img, (ball2x, ball2y), 50)
+
+      cv2.imshow('Ronnals Game', img)
 
 
 
-#----------------------------------------
+#---------------------------------------------------------------------------
 #main
 print('starting OO virtual3d.')# baby step to make sure machine is working
 ff = FaceFinder()#creating facefinder instance
 
-cap = cv2.VideoCapture(cv2.CAP_ANY)
+stage = Stage()#responsible for rendering
+
+img = np.zeros([1080,1920,3])#ndarray that initializes with zeros
+
+cv2.imshow('Game', img) #initialize game window
+
+cap = cv2.VideoCapture(cv2.CAP_ANY)# initialize web cam
 
 if not cap.isOpened():
-  print("Couldn't open cam")
+  print("Couldn't open camera. Make sure you have a web camera that is not being used by another app.")
   exit()
 
+moved = False
+while True:
+  # Reads the frame
+  ret, frame = cap.read()
 
+  if not ret:
+    print("Error reading the frame. Exiting...")
 
+  facexy = ff.find_face(frame)
+  frame_small = cv2.resize(frame, (frame.shape[1]//4, frame.shape[0]//4), interpolation= cv2.INTER_LINEAR)
+  cv2.imshow('q to quit', frame_small) #debugs, creates seperate ha;f-sized frame
 
+  if not moved:
+   cv2.moveWindow('q to quit',1080,0) #Forces frame to go to top left corner(1080,0)
 
-while True: 
-  retval,frame = cap.read()
-  if retval == False:
-    print("camera error!")
+  if facexy is not None: #if no face is seen, do not update the game 
+   img = stage.update(facexy)
 
-  ff.find_face(frame)
-  cv2.imshow('q to quit', frame)
-
+  # Stop if q key is pressed
   if cv2.waitKey(30) == ord('q'):
     break
+
+
+# Release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
+
+print('virtual3d complete')
+#while True: 
+#  retval,frame = cap.read()
+#  if retval == False:
+#    print("camera error!")
+
+#  ff.find_face(frame)
+#  cv2.imshow('q to quit', frame)
+
+#  if cv2.waitKey(30) == ord('q'):
+#    break
 
 #pause = input('press enter to end')
 
 
 #destroy cam
-cap.release()
+#cap.release()
 
-cv2.destroyAllWindows()
-print('virtual3d complete')
+#cv2.destroyAllWindows()
+
